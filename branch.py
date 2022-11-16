ec414b74686b2970aacb6741034ed0be8c647a54
stevec-doj-dev <stevec.doj.dev@gmail.com>
2022-11-16 12:54:55 -0500

    Added more get and set methods
    Started Branch

--- branch.py
+++ branch.py
@@ -1,2 +1,52 @@
 import node
 
+class branch:
+
+    branchLen = 0 #Length of branch between nodes
+    branchConnected = False #False if branch is not connected to 2 nodes, True if it is
+    branchN1 = None #First node connected
+    branchN2 = None #Second node connected
+    
+    def __init__(self, n1, n2):
+        self.branchN1 = n1
+        self.branchN2 = n2
+        self.branchLen = n1.getDistance(n2)
+        self.branchConnected = True
+    
+    ### Getters ###
+    #Return length of branch
+    def getLen(self):
+        return self.branchLen
+    
+    #Return node 1
+    def getN1(self):
+        return self.branchN1
+
+    #Return node 2
+    def getN2(self):
+        return self.branchN2
+
+    #Return if branch is fully connected
+    def getConnect(self):
+        return self.branchConnected
+
+    def __str__(self):
+        result = "Branch: " + str(self.branchLen)
+        return result
+    
+
+#Debug
+def main():
+    testNode = node.node(1,2,3)
+    t2 = node.node(5,5,6)
+    t3 = node.node(7,8,9)
+    testNode.addNode(t2)
+    testNode.addNode(t3)
+    #print(testNode)
+    #print(testNode.getDistance(t2))
+
+    testBranch = branch(testNode, t2)
+    print(testBranch.branchLen)
+
+if __name__ == '__main__': #if running node
+    main()
--- node.py
+++ node.py
@@ -1,3 +1,10 @@
+#Author: Stephen Cheney
+
+#node class
+
+import branch
+import math
+
 class node:
 
     xCoord = 0 #X coordinate of the node
@@ -5,12 +12,16 @@ class node:
     beneficialCoE = 0 #Beneficial Coefficient determines healthiness of the node to the virus
     visitedBool = False #Whether or not the node has been visited already
     numVisits = 0 #How many times the node has been visited
+    nodeList = [] #List of directly connected nodes
+    nodeBranches = []
 
     def __init__(self,x, y, BCE):
         self.xCoord = x
         self.yCoord = y
         self.beneficialCoE = BCE
+        self.nodeList = []
 
+    ### Getters ###
     #Get X Coordinate
     def getX(self):
         return self.xCoord
@@ -23,11 +34,73 @@ class node:
     def getBCE(self):
         return self.beneficialCoE
 
+    #Get num nodes connected
+    def getNodeCount(self):
+        return len(self.nodeList)
+
+    #Get list of all nodes connected
+    def getNodeList(self):
+        return self.nodeList
+
+    #Get the distance between 2 nodes
+    def getDistance(self, n2):
+        assert(type(self) is node),"Error: first object is not a node"
+        assert(type(n2) is node),"Error: second object is not a node"
+        distance = 0
+        n1x = self.getX()
+        n1y = self.getY()
+        n2x = n2.getX()
+        n2y = n2.getY()
+        distance = math.sqrt((n2x-n1x)**2 + (n2y-n1y)**2)
+        return distance
+
+    ### Setters ###
+    #Set X Coordinate
+    def setX(self, x):
+        assert(type(x) is int),"Error: coordinate must be int"
+        self.xCoord = x
+    
+    #Set Y Coordinate
+    def setY(self, y):
+        assert(type(y) is int),"Error: coordinate must be int"
+        self.yCoord = y
+
+    #Set Beneficial Coefficient
+    def setBCE(self, BCE):
+        assert(type(BCE) is float),"Error: BCE must be float"
+        self.beneficialCoE = BCE
+    
+    #Add a node to the connected node list; only immediately connected nodes
+    def addNode(self, N):
+        assert(type(N) is node),"Error: added object is not a node"
+        self.nodeList.append(N)
+        self.nodeBranches.append(branch.branch(self,N))
+
+    
+    def __str__(self):
+        result = "---------Node---------\nX coord:"+ str(self.getX())+ "\nY coord:"+ str(self.getY())+ "\nBeneficial CoE:"+ str(self.getBCE())+ "\nConnected Nodes: X  Y  Distance\n"
+        index = 0
+        for each in self.getNodeList():
+            result += "                 " + str(each.getX()) + ", " + str(each.getY()) + "  " + str(each.nodeBranches[index].getLen()) +"\n"
+            index += 1
+        return result
+
+    def strXY(self):
+        result = "Node: " + str(self.getX()) + ", " + str(self.getY())
+        return result
 
 #Debug
 def main():
     testNode = node(1,2,3)
-    print('X:', testNode.getX(),'Y:', testNode.getY(),'BCE:',testNode.getBCE())
+    #print('X:', testNode.getX(),'Y:', testNode.getY(),'BCE:',testNode.getBCE())
+    t2 = node(5,5,6)
+    t3 = node(7,8,9)
+    testNode.addNode(t2)
+    testNode.addNode(t3)
+    print(testNode)
+
+    #print(testNode.getDistance(t2))
+    print(testNode.strXY())
 
 if __name__ == '__main__': #if running node
     main()
